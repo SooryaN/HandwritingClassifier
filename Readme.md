7df2cd7624de0abf95037b63c98b4449bb748c9c
Soorya Narayan <soorya.art@gmail.com>
2016-01-25 21:41:05 +0530

    Tested against custom images

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
-# Handwriting Classification 
-
+# Digit Classifier 
+   
 This is an in progress webapp to implement a neural network that classifies handwritten digits from the MNIST dataset using python with numpy 
+   
+`$ python -i test.py`  
+`>>> image_to_number('sample_images/4.png')`  
+  
+As of now, you can run test.py in interactive mode and call the image_to_number() function to test against any sample images. 
--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 import data_loader as d
 import network
+from PIL import Image
 training_data, validation_data, test_data = d.load_data_wrapper()
 net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 2.3, test_data=test_data)
+net.SGD(training_data, 22, 10, 3.0, test_data=test_data)
+
+def image_to_number(location):
+    x=Image.open(location,'r').convert('L')
+    x.size
+    y=np.asarray(x.resize((28,28)).getdata(),dtype=np.float64).reshape((784,1))
+    y = 255-y
+    y = y/256
+    return np.argmax(net.feedforward(y))
